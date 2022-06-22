0b1184bf0290ad0a68ae18c483e2605083f4cc6d
Andrea Restrepo <andrearestrepox@gmail.com>
2022-06-22 14:21:55 -0400

    randomizing lyrics and genres

--- index.js
+++ index.js
@@ -4,48 +4,104 @@ let lyrics = [
     {
         lyric: "I've lost my mind, I've spent the night crying on the floor of my bathroom But you're so unaffected, I really don't get it",
         song: "Good 4 U",
-        genre: "POP",
+        genre: "Today's Hits",
+        albumArt: './Images/olivia rod album art.webp' ,
+        sound: ''
+    },
+    {
+        lyric: "I've never been a fan of change But I'd follow you to any place If it's Hollywood or Bishopsgate I'm coming too If you're feelin' down I just wanna make you happier, baby",
+        song: "Late Night Talking",
+        genre: "Today's Hits",
         albumArt: '' ,
         sound: ''
     },
     {
-        lyric: "They said, I bet they'll never make itBut just look at us holding on We're still together, still going strong",
-        song: "You're Still the One",
-        genre: "Country",
-        albumArt:'',
+        lyric: "There ain't no room for things to change When we are both so deeply stuck in our ways You can't deny how hard I have tried",
+        song: "Easy on Me",
+        genre: "Today's Hits",
+        albumArt: '' ,
         sound: ''
     },
     {
-        lyric: "East side to the west side Pushin' phat rides, it's no surprise She got tricks in the stash, stacking up the cash",
-        song: "No Diggity",
-        genre: "RnB",
-        albumArt:'',
+        lyric: "Aren't you the guy who tried to Hurt me with the word goodbye? Though it took some time to survive you I'm better on the other side I'm all good already So moved on, it's scary I'm not where you left me at all, so",
+        song: "Don't Start Now",
+        genre: "Today's Hits",
+        albumArt: '' ,
         sound: ''
     },
     {
-        lyric: "She's got eyes of the bluest skies As if they thought of rain I'd hate to look into those eyes and see an ounce of pain",
-        song: "Sweet Child O' Mine",
-        genre: "Rock",
-        albumArt:'',
+        lyric: "So don't waste the time I don't have And don't try to make me feel bad I could talk about every time that you showed up on time But I'd have an empty line 'cause you never did",
+        song: "Happier Than Ever",
+        genre: "Today's Hits",
+        albumArt: '' ,
         sound: ''
     },
-    {
-        lyric: "And see, people, they don't understand Your girlfriends, they can't understand Your grandsons, they won't understand On top of this, I ain't ever gonna understand",
-        song: "Last Nite",
-        genre: "Indie",
-        albumArt:'',
-        sound: ''
-    },
-    {
-        lyric: "4 de la mañana, Y Andrea Saliendo de La Perla Muchos la critican sin conocerla Calle como un Civic Inteligente como un Tesla",
-        song: "Andrea",
-        genre: "Latin",
-        albumArt:'',
-        sound: ''
-    },
+    // //{
+    //     lyric: "They said, I bet they'll never make itBut just look at us holding on We're still together, still going strong",
+    //     song: "You're Still the One",
+    //     genre: "Country",
+    //     albumArt:'',
+    //     sound: ''
+    // },
+    // {
+    //     lyric: "East side to the west side Pushin' phat rides, it's no surprise She got tricks in the stash, stacking up the cash",
+    //     song: "No Diggity",
+    //     genre: "RnB",
+    //     albumArt:'',
+    //     sound: ''
+    // },
+    // {
+    //     lyric: "She's got eyes of the bluest skies As if they thought of rain I'd hate to look into those eyes and see an ounce of pain",
+    //     song: "Sweet Child O' Mine",
+    //     genre: "Rock",
+    //     albumArt:'',
+    //     sound: ''
+    // },
+    // {
+    //     lyric: "And see, people, they don't understand Your girlfriends, they can't understand Your grandsons, they won't understand On top of this, I ain't ever gonna understand",
+    //     song: "Last Nite",
+    //     genre: "Indie",
+    //     albumArt:'',
+    //     sound: ''
+    // },
+    // {
+    //     lyric: "4 de la mañana, Y Andrea Saliendo de La Perla Muchos la critican sin conocerla Calle como un Civic Inteligente como un Tesla",
+    //     song: "Andrea",
+    //     genre: "Latin",
+    //     albumArt:'',
+    //     sound: ''
+    // },
     {
         lyric: "I don't understand Why do I stress the man? When there's so many bigger things at hand We coulda never had it all We had to hit a wall So this is inevitable withdrawal",
         song: "Tears Dry on Their Own",
+        genre: "2000s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "City's breaking down on a camel's back They just have to go, 'cause they don't know wack So while you fill the streets, it's appealing to see You won't get out the county, 'cause you're bad and free You got a new horizon, it's ephemeral style A melancholy town where we never smile",
+        song: "Feel Good Inc",
+        genre: "2000s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "Past and present, they don't matter Now the future's sorted out Watch her moving in elliptical pattern Think it's not what you say What you say is way too complicated",
+        song: "1901",
+        genre: "2000s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "Don't try to fight the feeling Because the thought alone is killin' me right now (uh) Thank God for Mom and Dad For sticking two together",
+        song: "Hey Ya!",
+        genre: "2000s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "Sometimes I get the feeling She's watchin' over me And other times I feel like I should go And through it all, the rise and fall The bodies in the streets And when you're gone, we want you all to know",
+        song: "Welcome to the Black Parade",
         genre: "2000s",
         albumArt:'',
         sound: ''
@@ -58,8 +114,64 @@ let lyrics = [
         sound: ''
     },
     {
+        lyric: "Flip on the tell Wrestle with Jimmy Something is bubbling Behind my back The bottle is ready to blow",
+        song: "Say It Ain't So",
+        genre: "90s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "Well, I had a million dollars but I'd, I'd spend it all If I could find that Heina and that Sancho that she's found Well, I'd pop a cap in Sancho and I'd slap her down",
+        song: "Santeria",
+        genre: "90s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "And backbeat, the word is on the street that the fire in your heart is out I'm sure you've heard it all before, but you never really had a doubt I don't believe that anybody feels the way I do about you now",
+        song: "Wonderwall",
+        genre: "90s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "It was all a dream, I used to read Word Up! magazine Salt-n-Pepa and Heavy D up in the limousine Hangin' pictures on my wall Every Saturday Rap Attack, Mr. Magic, Marley Marl",
+        song: "Juicy",
+        genre: "90s",
+        albumArt:'',
+        sound: ''
+    },
+    {
         lyric: "It's the terror of knowing what this world is about Watching some good friends screaming, Let me out - Pray tomorrow gets me higher, higher, high Pressure on people, people on streets",
         song: "Under Pressure",
+        genre: "80s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "Me and some guys from school Had a band and we tried real hard Jimmy quit and Jody got married I should've known we'd never get far",
+        song: "Summer Of '69",
+        genre: "80s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: " ain't got many friends left to talk to Nowhere to run when I'm in trouble You know I'd do anything for you Stay the night but keep it undercover",
+        song: "Your Love",
+        genre: "80s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "'ll play along with the charade There doesn't seem to be a reason to change You know, I feel so dirty when they start talking cute I wanna tell her that I love her, but the point is probably moot",
+        song: "Jessie's Girl",
+        genre: "80s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "Though I keep searching for an answer I never seem to find what I'm looking for Oh Lord, I pray you give me strength to carry on 'Cause I know what it means To walk along the lonely street of dreams",
+        song: "Here I Go Again",
         genre: "80s",
         albumArt:'',
         sound: ''
@@ -71,22 +183,113 @@ let lyrics = [
         albumArt:'',
         sound: ''
     },
+    {
+        lyric: "Maybe they're blinded But Bennie makes them ageless We shall survive, let us take ourselves alone Where we fight our parents out in the streets",
+        song: "Bennie and The Jets",
+        genre: "70s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "You walked into the party like you were walking onto a yacht Your hat strategically dipped below one eye",
+        song: "You're so Vain",
+        genre: "70s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "We take all the action we can meet And we cover all the northeast state When the strip shuts down we run 'em in the street",
+        song: "Racing in the Street",
+        genre: "70s",
+        albumArt:'',
+        sound: ''
+    },
+    {
+        lyric: "Well, you can tell by the way I use my walk I'm a woman's man, no time to talk Music loud and women warm, I've been kicked around",
+        song: "Stayin' Alive",
+        genre: "70s",
+        albumArt:'',
+        sound: ''
+    },
     
 ]
 
+//MAIN GAME//
+
+// function genreSelect(event){
+//     let genreArray = [];
+//     let selectedGenre = event.target.innerHTML
+//     console.log(selectedGenre)
+// }
+
+function genreSelect(selectedGenre) {
+let genreArray = lyrics.filter(singleLyric => {
+    return singleLyric.genre === selectedGenre;
+})
+return genreArray;
+}
+
+
+
+
+
+// more concise way
+//  let filteredArray = genreSelect("2000s")
+//  return filteredArray[Math.floor(Math.random() * filteredArray.length)].lyric
+
+document.getElementById("2000s-btn").onclick =  () => {
+    let filteredArray = genreSelect("2000s");
+    let randomIndex = Math.floor(Math.random() * filteredArray.length);
+    let selectedLyric = filteredArray[randomIndex].lyric;
+    let lyricDisplay = document.querySelector('h1');
+    //console.log(document.querySelector('h1'))
+     //console.log("lyricDisplay ==>", lyricDisplay)
+    lyricDisplay.innerHTML = selectedLyric
+    //console.log("lyricDisplay.innerHTML -->", lyricDisplay.innerHTML)   
+}
+document.getElementById("todays-btn").onclick =  () => {
+    let filteredArray = genreSelect("Today's Hits");
+    let randomIndex = Math.floor(Math.random() * filteredArray.length);
+    let selectedLyric = filteredArray[randomIndex].lyric;
+    let lyricDisplay = document.querySelector('h1');
+    //console.log(document.querySelector('h1'))
+     //console.log("lyricDisplay ==>", lyricDisplay)
+    lyricDisplay.innerHTML = selectedLyric
+}
+
+
+
+
+// document.getElementById("todays-btn").onclick = genreSelect() 
+// document.getElementById("90s-btn").onclick = genreSelect()
+// document.getElementById("80s-btn").onclick = genreSelect() 
+// document.getElementById("70s-btn").onclick = genreSelect()
+
+
 
 //timer section//
+let timer = 5;
+function startQuiz() {
+    function countdownTimer() {
+        //const intervalID = setInterval(() => {
+                if(timer > 0){
+                    timer = timer -1
+                    console.log(timer)
+                }else{
+                    return "times up!"
+                }
+                return `${timer} seconds remaining`
+            //}, 
+           // 1000)
+        }
+    const intervalID = setInterval(() => {
+        const timerDisplay = document.getElementById("circle")
+        timerDisplay.innerHTML = countdownTimer()
+        console.log("buttonClicked")
+    },1000)
+}
+//timer section//
 
-let timeleft = 25;
-let downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementsById("circle").innerHTML = "Times Up!";
-  } else {
-    document.getElementById("circle").innerHTML = timeleft + " " + "seconds remaining";
-  }
-  timeleft -= 1;
-}, 1000);
 
 
 
